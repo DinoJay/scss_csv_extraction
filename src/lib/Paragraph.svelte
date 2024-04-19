2230ad00b38c71686e591925e819fe7e258beea9
DinoJay <jott88@googlemail.com>
2024-04-19 13:49:45 +0200

    yeah

--- src/lib/Paragraph.svelte
+++ src/lib/Paragraph.svelte
@@ -9,19 +9,18 @@
 
 	let loadingResponse = false;
 
-	let question = '';
 	let response = null;
+	let chatGPTerror;
 
 	const openai = new OpenAI({
 		apiKey: import.meta.env.VITE_OPEN_AI,
 		dangerouslyAllowBrowser: true
 	});
 
-	let variables = [];
-	let selectedEndpoint = endpoints[0].name;
-
-	$: cols = endpoints.find((d) => d.name === selectedEndpoint)?.cols.join(',');
-	$: colsText = `create a table with html table element and no other text with the following columns and extract the information from the text: ${cols}`;
+	const genQuery = (cols) =>
+		`create a html table (using only the table element) with following columns finding the information for the rows in the text given. don't include any commentary text: ${cols}`;
+	let selEndpoint = endpoints[0].name;
+	let question = genQuery(endpoints[0].cols);
 
 	$: setChatGPTContext = (array) => {
 		const messages = array.map((p) => ({
@@ -50,6 +49,13 @@
 				loadingResponse = false;
 				console.log('d', d);
 				response = d;
+				// match(/```html\n([\s\S]*?)\n```/g)[0]
+				console.log('response', response.choices[0].message.content);
+				chatGPTerror = null;
+			})
+			.catch((err) => {
+				chatGPTerror = err;
+				// console.log(error);
 			});
 	};
 </script>
@@ -84,39 +90,49 @@
 			<div class="mt-3">
 				<div class="flex gap-2">
 					{#each endpoints as e}
-						<button class="p-2 border-2 flex-1">{e.name}</button>
+						<button
+							class="p-2 border-2 flex-1"
+							class:bg-blue-300={e.name === selEndpoint}
+							on:click={() => {
+								selEndpoint = e.name;
+								question = genQuery(e.cols);
+							}}>{e.name}</button
+						>
 					{/each}
 				</div>
 
 				<div class=" flex my-2">
-					<input
+					<textarea
 						on:change={(e) => {
 							question = e.target.value;
 						}}
+						value={question}
 						placeholder="ChatGPT question"
-						type="text"
-						class="border-2 border-gray p-2 flex-grow"
+						class="h-32 border-2 border-gray p-2 flex-grow"
 					/>
 				</div>
 				<button
 					class="w-full p-2 border-2"
-					on:click={() => setChatGPTContext([text, colsText])}
+					on:click={() => setChatGPTContext([text, question])}
 					type="button"
 				>
 					Submit
 				</button>
 			</div>
-			{#if loadingResponse}
+			<div class="w-full flex min-h-44 overflow-auto">
 				<div class="w-full flex min-h-44">
-					<div class="m-auto">Loading...</div>
+					{#if chatGPTerror !== null}
+						{chatGPTerror}
+					{/if}
+					{#if loadingResponse}
+						<div class="m-auto">Loading...</div>
+					{:else if response}
+						<p class="bg mt-2 p-1" style:background="#cae6ea">
+							{@html response.choices[0].message.content}
+						</p>
+					{/if}
 				</div>
-			{:else if response}
-				<div class="w-full flex min-h-44 overflow-auto">
-					<p class="bg mt-2 p-1" style:background="#cae6ea">
-						{@html response.choices[0].message.content}
-					</p>
-				</div>
-			{/if}
+			</div>
 		</div>
 	{/if}
 </div>
--- src/lib/endpoints.js
+++ src/lib/endpoints.js
@@ -2,4 +2,36 @@ export default [
     {
         name: 'test substance',
         cols: ['homogeneity and stability', 'pH', 'physical form (solid, liquid)', 'achieved concentration (%)', 'composition', 'purity (%)', 'chemical batch nr', 'additional info']
-    },]
+    },
+    {
+        name: "vehicle", cols: ['name', 'concentration (%)']
+    },
+    {
+        name: 'test condition', cols: [
+            'nominal dose levels',
+            'actual dose levels',
+            'dose level unit',
+            'dose volume ml / kg bw',
+            'repeated administration scheme',
+            'recovery period'
+        ]
+    },
+    {
+        name: 'oral administration', cols: ['gavage', 'diet', 'drinking water']
+    },
+    {
+        name: 'test animal',
+        cols: [
+            'rodent species/strain',
+            'source',
+            'age at start of experiment',
+            'age unit',
+            'sex',
+            'housing and feeding',
+            'weight',
+            'weight unit',
+            'n animals / group',
+            'number of satellite group'
+        ]
+    },
+]
